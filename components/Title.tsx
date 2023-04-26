d6ef4bb35a758dd521449280d753ce6f3345ce04
craviottoalex <craviottoalex@gmail.com>
2023-04-26 18:25:04 +0200

    Main header

---
+++ components/Title.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    
+    return (
+        <main>
+            <h1 class='text-5xl font-light text-left mb-4 lg:text-6xl'>Alex<br/>Craviotto</h1>
+            <h2 class='text-xs lg:text-base font-extralight'>Computer engineer and passionate self-taught. </h2>
+        </main>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Gilda_Display } from 'next/font/google'
+
+const gildaDisplay = Gilda_Display({ 
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--gilda-display-font'
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
