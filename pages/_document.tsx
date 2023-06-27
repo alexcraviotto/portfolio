d4a4c09101e660f1f646251d14ec88830c056d93
Alex Craviotto <craviottoalex@gmail.com>
2023-06-27 23:49:03 +0200

    Logo added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <title>Alex Craviotto - Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
